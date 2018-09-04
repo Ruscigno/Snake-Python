472a656bd13448bbb98ed9a9592dacfb41e4b4e0
Sander Ruscigno <ruscigno@gmail.com>
2018-09-03 21:49:30 -0300

    Part 1

---
+++ play-snake.py
@@ -0,0 +1,12 @@
+# Simple Snake Game in Python 3
+# By Sander Ruscigno
+import turtle
+
+# set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game By @Ruscigno")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0) #turns off animation
+
+wn.mainloop()
